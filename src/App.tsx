f8fa86d01e30e7c8f29baca124da74599360f94a
chary <chary@dfd.com>
2026-03-31 16:32:49 +0500

    fix: fix pass

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { EboutUsPage } from "./pages/EboutUsPage/EboutUsPage";
 import { ItemPage } from "./pages/ItemPage/ItemPage";
 import { SellPage } from "./pages/SellPage/SellPage";
 import { UserPage } from "./pages/UserPage/UserPage";
-import { Meinheder } from "./component/meinheder/meinheder";
+import { Meinheder } from "./component/meinheder/Meinheder";
 
 function App() {
   return (
     <Routes>
       <Route element={<Meinheder />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/ebout" element={<EboutUsPage />} />
+        <Route path="/about" element={<EboutUsPage />} />
         <Route path="/item" element={<ItemPage />} />
         <Route path="/sell" element={<SellPage />} />
         <Route path="/user" element={<UserPage />} />
